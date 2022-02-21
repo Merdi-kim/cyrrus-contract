08c77d338eb4290753b0295898924d5294c6d8c2
Merkim <merkim@Merkims-MacBook-Pro.local>
2022-02-21 06:30:57 +0300

    change file reading mechanism

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require("fs") 
-const privateKey = fs.readFileSync("secret").trim().toString()
+const privateKey = fs.readFileSync("secret").toString()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
